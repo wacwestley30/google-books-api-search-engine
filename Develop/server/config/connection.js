a10abc89a52e48d7c8c20dd966867d835d4367f1
wacwestley30 <westleycervantes@gmail.com>
2024-05-23 19:05:59 -0400

    updated server/connection to handle SSL handleshake with Render

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true, // Force TLS
+  tlsAllowInvalidHostnames: true, // Allow invalid hostnames (e.g., localhost)
+});
 
 module.exports = mongoose.connection;
